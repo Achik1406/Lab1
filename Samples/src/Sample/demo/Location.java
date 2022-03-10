c6796f5c79551a1870ceafa7ae7bfc315743b015
Muhammad Zulfikri <zckfikri@gmail.com>
2022-03-10 00:02:37 -0800

    location commit

---
+++ Samples/src/Sample/demo/Location.java
@@ -0,0 +1,15 @@
+package Sample.demo;
+
+public class Location {
+
+	private int attribute;
+
+	public int getAttribute() {
+		return attribute;
+	}
+
+	public void setAttribute(int attribute) {
+		this.attribute = attribute;
+	}
+	
+}
